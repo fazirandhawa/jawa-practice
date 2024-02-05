713e05e9e606ef7b602a2b6731af79091974e268
fazirandhawa <155283967+fazirandhawa@users.noreply.github.com>
2024-02-05 19:20:21 +0500

    cloned

---
+++ practice.js
@@ -0,0 +1,53 @@
+// prompt("hello");
+
+
+//                                 ........... <<<<<<<prtice test   1  >>>>>>>>>>............
+
+
+// const product ={
+//     title : "ballpen",
+//     rating : 4,
+//     offer : 5 ,
+//     price : 220,
+// };
+// console.log(product);
+// console.log(typeof product)
+
+//                                ........... <<<<<<<prtice test  2   >>>>>>>>>>............
+
+// ({QUESTION})...............get input by user by prompt and cheak is it a multiple of 5 or not 
+
+// let a=prompt("enter a number");
+// console.log( a); 
+// if(a%5===0){
+//     console.log( a,"is multiple of 5")
+// }else{
+//     console.log(a,"is not multiple of 5")
+// }
+
+
+//  {(QUESTION NO 3 )}            ........... <<<<<<<prtice test  2 >>>>>>>>>>............
+
+
+//        ............................. GRADING OF STUDENT MARKS................
+
+let grade;
+let num=75;
+
+// let num=prompt("enter the marks of student ");
+
+
+// console.log(num,"is marks of student ");
+if(num>=80 && num<=100){
+    grade="A";
+} elseif(num>=70 && num<80) {
+    grade="B";
+ } elseif(num>=60 && num<69) {
+    grade="C";
+} elseif(num>=50 && num<59) {
+    grade="D";
+} ifelse {
+    grade="F";
+}
+
+console.log("according to your score,your grade is ",grade);
